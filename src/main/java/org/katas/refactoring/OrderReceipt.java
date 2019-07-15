6df35bc9a028a8042da8620eded60f95e1ffaec2
luhaojian11 <962671114@qq.com>
2019-07-15 20:20:11 +0800

    review class OrderReceipt : delete the annotation and use String.format() instead of append() continuously.

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -15,31 +15,19 @@ public class OrderReceipt {
 
     public String printReceipt() {
         StringBuilder receipt = new StringBuilder();
-
-        receipt.append("======Printing Orders======\n");
-        receipt.append(order.getCustomerName());
-        receipt.append(order.getCustomerAddress());
-
+        receipt.append(String.format("======Printing Orders======\n%s%s", order.getCustomerName(), order.getCustomerAddress()));
         double totalSalesTax = 0d;
         double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             receipt.append(String.format("%s\t%s\t%s\t%s\n",
                     lineItem.getDescription(), lineItem.getPrice(),
                     lineItem.getQuantity(), lineItem.totalAmount()));
-
-            // calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
             totalSalesTax += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             totalAmount += lineItem.totalAmount() + salesTax;
         }
-
-        // prints the state tax
-        receipt.append("Sales Tax").append('\t').append(totalSalesTax);
-
-        // print total amount
-        receipt.append("Total Amount").append('\t').append(totalAmount);
+        receipt.append(String.format("Sales Tax\t%s", totalSalesTax));
+        receipt.append(String.format("Total Amount\t%s", totalAmount));
         return receipt.toString();
     }
 }
